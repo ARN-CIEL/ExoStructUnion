835d6743e5cb3cdd745e5e66e287645cf0ceae46
Nathan Arnould <nathansssarnould@gmail.com>
2024-11-28 10:51:28 +0100

    #2 Déclaration des variables

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
+
+// Énumération pour l'état du contenu
+enum EEtat { vide, palette, rouleau};
+
+// Union pour le contenu du casier
+union UContenu {
+    SPalette palette; 
+    SRouleau rouleau;
+
+};
+
+// Structure pour Palette
+struct SPalette {
+    int poids; 
+    char reference[20];
+};
+
+// Structure pour Rouleau
+struct SRouleau {
+    float longueur; 
+    int numero;
+};
+
+// Structure pour Casier
+struct SCasier {
+    int x; 
+    int y; 
+    EEtat etat; 
+    UContenu contenu;
+
+};
+
+SCasier transgerbeur[9];
+
+
 
 int main()
 {
